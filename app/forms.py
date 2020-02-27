e5b0e83245a50718d85bc7518deffec6072bba5d
bxtkezhan <bxtkezhan@qq.com>
2020-02-27 23:02:39 +0800

    add login and register

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
+from .models import User
+from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, Length, EqualTo
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(8, 24)])
-    submit = SubmitField('Submit')
+    remember = BooleanField('Remember', default=False)
+    submit = SubmitField('Login')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 24)])
+    password2 = PasswordField('Repead Password', validators=[DataRequired(), Length(8, 24), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app import config
-from .forms import LoginForm
+from app import db
+from .forms import LoginForm, RegisterForm
+from .models import User
 
 
 @app.route('/')
@@ -11,13 +14,42 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        flash(
-            'Login requested Username: {}, Password: {}'.format(username, password))
-        print(flash)
+        remember = form.remember.data
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(password):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=remember)
         return redirect(url_for('index'))
-
     return render_template('login.html', title=config.title, form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        user = User(username=username, email=email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title=config.title, form=form)
